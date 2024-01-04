ebbb1108e1cf8508b1f869770be1c8c84f9e0d3c
Vivitthachai <vivitachaigood@hotmail.com>
2024-01-04 17:22:09 +0700

    store in MongoDB

--- stack/stack/pipelines.py
+++ stack/stack/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
 
-class StackPipeline:
+class MongoDBPipeline:
+    def __init__(self) -> None:
+        self.conn = pymongo.MongoClient(
+            host="localhost",
+            port=27017
+        )
+        #connect to the database
+        db = self.conn['stackoverflow']
+        self.collection = db['questions']
+        
     def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
         return item
+    
+    def close_spider(self, spider):
+        self.conn.close()
--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -62,9 +62,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "stack.pipelines.StackPipeline": 300,
-#}
+#refer to stack folder -> pipeline.py -> class MongoDBPipeline
+ITEM_PIPELINES = {
+    "stack.pipelines.MongoDBPipeline": 300,
+}
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
